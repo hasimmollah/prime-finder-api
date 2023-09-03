a2400085a45c66698d876fd6d7ad85e54c441939
hasimmollah <hasim.mollah@gmail.com>
2023-09-03 12:58:05 +0100

    Externalize properties

--- src/main/java/com/nw/primefinder/Application.java
+++ src/main/java/com/nw/primefinder/Application.java
@@ -1,5 +1,7 @@
 package com.nw.primefinder;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,10 +10,28 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static java.lang.System.exit;
+
 @SpringBootApplication
 @EnableSwagger2
+@Slf4j
 public class Application {
+    @SneakyThrows
     public static void main(String[] args) {
+        try (InputStream in = args.length>0? new FileInputStream(args[0]): Application.class.getResourceAsStream("/conf.properties")) {
+            Properties properties = new Properties();
+            properties.load(in);
+            properties.stringPropertyNames().forEach(key->{
+                System.setProperty(key, properties.getProperty(key));
+            });
+        } catch(Exception e){
+            log.error("Failed to start the application", e);
+            exit(1);
+        }
         SpringApplication.run(Application.class, args);
     }
     @Bean
--- src/test/java/com/nw/primefinder/service/helper/AbstractStrategyTest.java
+++ src/test/java/com/nw/primefinder/service/helper/AbstractStrategyTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -25,36 +27,38 @@ abstract class AbstractStrategyTest {
         assertTrue(getPrimesExpected.containsAll(getPrimesActual));
     }
 
+    private static GetPrimeRequest prepareGetPrimeRequest(int initial){
+         return GetPrimeRequest.builder()
+                 .initial(initial)
+                 .build();
+     }
+
     private static Stream<Arguments> testInputProvider() {
         int initial19 = 19;
         List<Integer> primesTill19 = List.of(2, 3, 5, 7, 11, 13, 17, 19);
 
-        GetPrimeRequest getPrimeRequest19 = GetPrimeRequest.builder()
-                .initial(initial19)
-                .build();
+        GetPrimeRequest getPrimeRequest19 =prepareGetPrimeRequest(initial19);
 
         int initial20 = 20;
         List<Integer> primesTill20 = List.of(2, 3, 5, 7, 11, 13, 17, 19);
 
-        GetPrimeRequest getPrimeRequest20 = GetPrimeRequest.builder()
-                .initial(initial20)
-                .build();
+        GetPrimeRequest getPrimeRequest20 = prepareGetPrimeRequest(initial20);
 
         int initial22 = 22;
         List<Integer> primesTill22 = List.of(2, 3, 5, 7, 11, 13, 17, 19);
 
-        GetPrimeRequest getPrimeRequest22 = GetPrimeRequest.builder()
-                .initial(initial22)
-                .build();
+        GetPrimeRequest getPrimeRequest22 = prepareGetPrimeRequest(initial22);
 
         int initial23 = 23;
         List<Integer> primesTill23 = List.of(2, 3, 5, 7, 11, 13, 17, 19, 23);
 
-        GetPrimeRequest getPrimeRequest23 = GetPrimeRequest.builder()
-                .initial(initial23)
-                .build();
+        GetPrimeRequest getPrimeRequest23 = prepareGetPrimeRequest(initial23);
 
         return Stream.of(
+                Arguments.of(prepareGetPrimeRequest(0), Collections.<Integer>emptyList()),
+                Arguments.of(prepareGetPrimeRequest(1),  Collections.<Integer>emptyList()),
+                Arguments.of(prepareGetPrimeRequest(2), List.of(2)),
+                Arguments.of(prepareGetPrimeRequest(3), List.of(2,3)),
                 Arguments.of(getPrimeRequest19, primesTill19),
                 Arguments.of(getPrimeRequest20, primesTill20),
                 Arguments.of(getPrimeRequest22, primesTill22),
