e2e2b123394b73d1bacae618b81686112c963e07
hasimmollah <hasim.mollah@gmail.com>
2023-09-03 17:20:56 +0100

    adding swagger

--- src/main/java/com/nw/primefinder/Application.java
+++ src/main/java/com/nw/primefinder/Application.java
@@ -4,11 +4,6 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -17,27 +12,20 @@ import java.util.Properties;
 import static java.lang.System.exit;
 
 @SpringBootApplication
-@EnableSwagger2
 @Slf4j
 public class Application {
     @SneakyThrows
     public static void main(String[] args) {
-        try (InputStream in = args.length>0? new FileInputStream(args[0]): Application.class.getResourceAsStream("/conf.properties")) {
+        try (InputStream in = args.length > 0 ? new FileInputStream(args[0]) : Application.class.getResourceAsStream("/conf.properties")) {
             Properties properties = new Properties();
             properties.load(in);
-            properties.stringPropertyNames().forEach(key->{
+            properties.stringPropertyNames().forEach(key -> {
                 System.setProperty(key, properties.getProperty(key));
             });
-        } catch(Exception e){
+        } catch (Exception e) {
             log.error("Failed to start the application", e);
             exit(1);
         }
         SpringApplication.run(Application.class, args);
     }
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.nw.primefinder.controller")).build();
-    }
-    
 }
